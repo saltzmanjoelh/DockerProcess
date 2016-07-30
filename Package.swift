810c1e0a51f56695723560967880f2a9edc6c94a
Joel Saltzman <talk@joelsaltzman.com>
2016-07-30 00:33:26 -0700

    Moved TaskExtension to it's own package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "DockerTask"
+    name: "DockerTask",
+    dependencies: [
+        .Package(url: "https://github.com/saltzmanjoelh/TaskExtension.git", versions: Version(0,0,0)..<Version(10,0,0))
+        ]
 )
