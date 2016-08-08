16d06661dc9da6d2b6bd7d11ee21daeaa3945c64
Joel Saltzman <talk@joelsaltzman.com>
2016-08-07 22:18:20 -0700

    added toolbox support

--- Sources/DockerTask.swift
+++ Sources/DockerTask.swift
@@ -1,12 +1,17 @@
 import Foundation
 import TaskExtension
+//TODO: split toolbox task out
+//TODO: protocols?
 
 public enum DockerTaskException: Error {
     case invalidConfiguration(message:String)
+    case toolbox(message:String)
+    
     var description : String {
         get {
             switch (self) {
             case let .invalidConfiguration(message): return message
+            case let .toolbox(message): return message
             }
         }
     }
@@ -18,12 +23,17 @@ public enum DockerTaskException: Error {
 //}
 
 public struct DockerTask {
+    public var launchPath = "/usr/local/bin/docker"
     public let command: String // run, exec, ps
     public let commandOptions: [String]?// --name
     public let imageName: String? // saltzmanojoelh/swiftubuntu
     //You have to be careful when populating this value. Everything after the -c is one string
     public let commandArgs: [String]?// ["/bin/bash", "-c", "echo something"]
     public var shouldSilenceOutput = false
+    
+    let machinePath = "/usr/local/bin/docker-machine"
+    let vBoxManagePath = "/Applications/VirtualBox.app/Contents/MacOS/VBoxManage"
+    let startupScript = "/Applications/Docker/Docker Quickstart Terminal.app/Contents/Resources/Scripts/start.sh"
     
     public init(command:String, commandOptions:[String]? = nil, imageName:String? = nil, commandArgs:[String]? = nil) {
         self.command = command
@@ -32,9 +42,6 @@ public struct DockerTask {
         self.commandArgs = commandArgs
     }
     
-    public var launchPath: String {
-        return ProcessInfo.processInfo.environment["DOCKER_PATH"] ?? "/usr/local/bin/docker"
-    }
     public var launchArguments: [String] {
         get {
             var arguments = [command]
@@ -50,30 +57,107 @@ public struct DockerTask {
             return arguments
         }
     }
-//    public func pullArguments() throws -> [String]  {
-//        guard let image = imageName else {
-//            throw DockerTaskException.invalidConfiguration(message:"You must set the imageName if you are going to pull")
-//        }
-//        return ["pull", image]
-//    }
-    
+    public func shouldPull(image:String) -> Bool {
+        let imagesResult = DockerTask(command:"images", commandOptions:["-a"]).launch(silenceOutput: true)
+        var images = [String]()
+        if let output = imagesResult.output {
+            images = output.components(separatedBy: "\n").filter{ $0.hasPrefix(image) }
+        }
+        return images.count == 0
+    }
     @discardableResult
     public func launch(silenceOutput:Bool = false) -> (output:String?, error:String?, exitCode:Int32) {
         //Make sure that the image has been pulled first. Otherwise, the error output gets filled with "Unable to find image locally..."
         if let image = imageName {
-            let imagesResult = DockerTask(command:"images", commandOptions:["-a"]).launch(silenceOutput: true)
-            if let output = imagesResult.output {
-                if output.components(separatedBy: "\n").filter({ (line:String) -> Bool in
-                    return line.hasPrefix(image)
-                }).count == 0 {
-                    DockerTask(command: "pull", commandOptions: [image]).launch()
-                }
-                
+            if shouldPull(image: image) {
+                DockerTask(command: "pull", commandOptions: [image]).launch()
             }
         }
         
-//        print("DockerTask Launching:\n\(launchPath) \(launchArguments.joined(separator: " "))")
+        //        print("DockerTask Launching:\n\(launchPath) \(launchArguments.joined(separator: " "))")
         
         return Task.run(launchPath:launchPath, arguments:launchArguments, silenceOutput:silenceOutput)
     }
+    
+    func validateToolboxPaths() throws {
+        
+        //        let startupScript = "/Users/asdf/Sites/Linux/DockerTask/start.sh"
+        let files = [("Docker Machine", machinePath), ("VBoxManage", vBoxManagePath), ("Docker Startup Script", startupScript)]
+        for file in files {
+            guard FileManager.default.fileExists(atPath: file.1) else {
+                throw DockerTaskException.toolbox(message: "\(file.0) is missing from path: \(file.1)")
+            }
+        }
+    }
+    func vmExists(name:String = "default") -> Bool {
+        let result = Task.run(launchPath:"/bin/bash", arguments: ["-c", "\(vBoxManagePath) list vms | grep \(name)"], silenceOutput: false)
+        return result.output != nil && result.output!.contains(name)
+    }
+    func vmIsRunning(name:String = "default") -> Bool {
+        let result = Task.run(launchPath:"/bin/bash", arguments: ["-c", "export PATH=/usr/local/bin:$PATH && \(machinePath) status \(name)"], silenceOutput: false)
+        return result.output != nil && result.output!.contains("Running")
+    }
+    func vmStart(name:String = "default") throws {
+        let result = Task.run(launchPath:"/bin/bash", arguments: ["-c", "export PATH=/usr/local/bin:$PATH && \(machinePath) start default"], silenceOutput: false)
+        guard result.output != nil else {
+            throw DockerTaskException.toolbox(message: "Failed to start default machine.\n\(result.error)")
+        }
+    }
+    
+    @discardableResult
+    public func launchFromToolbox(silenceOutput:Bool = false) throws -> (output:String?, error:String?, exitCode:Int32) {
+        if(!vmExists()){
+            //TODO: create VM
+        }
+        if(!vmIsRunning()){
+            try vmStart()
+        }
+        let command = "\(launchPath) \(launchArguments.joined(separator: " "))"
+        let export = "export PATH=/usr/local/bin:$PATH"
+        let environmentVars = "eval $(/usr/local/bin/docker-machine env --shell=bash default)"
+        let args = ["/bin/bash", "-c", "\(export); \(environmentVars); \(command)"]
+        
+//        print("DockerTask Launching:\n/usr/bin/env \(args.joined(separator: " "))")
+        return Task.run(launchPath:"/usr/bin/env", arguments: args, silenceOutput: false)
+    }
+    
+    /*
+    if [ $DOCKER_REQUIRES_LOGIN -eq 1 ]; then
+    #configure vars
+    VM=default
+    DOCKER_MACHINE=/usr/local/bin/docker-machine
+    VBOXMANAGE=/Applications/VirtualBox.app/Contents/MacOS/VBoxManage
+    unset DYLD_LIBRARY_PATH
+    unset LD_LIBRARY_PATH
+    
+    #verify apps exist
+    if [ ! -f $DOCKER_MACHINE ] || [ ! -f $VBOXMANAGE ]; then
+    echo "Either VirtualBox or Docker Machine are not installed. Please re-run the Toolbox Installer and try again." >> $LOG_FILE 2>&1
+    exit 1
+    fi
+    #verify vm exists
+    $VBOXMANAGE showvminfo $VM &> /dev/null
+    VM_EXISTS_CODE=$?
+    
+    #create and start if needed
+    if [ $VM_EXISTS_CODE -eq 1 ]; then
+    echo "Creating Machine $VM..." >> $LOG_FILE 2>&1
+    $DOCKER_MACHINE rm -f $VM &> /dev/null
+    rm -rf ~/.docker/machine/machines/$VM
+    $DOCKER_MACHINE create -d virtualbox --virtualbox-memory 2048 $VM
+    else
+    echo "Machine $VM already exists in VirtualBox." >> $LOG_FILE 2>&1
+    fi
+    echo "Starting machine $VM..." >> $LOG_FILE 2>&1
+    $DOCKER_MACHINE start $VM
+    
+    #prepare docker
+    echo "Machine started, logging in." >> $LOG_FILE 2>&1
+    eval "$(docker-machine env --shell=bash default)" > $LOG_FILE 2>&1
+    bash --login >> $LOG_FILE 2>&1
+    echo "Logged in, starting image ${DOCKER_IMAGE} and running \"$DOCKER_COMMAND\"" >> $LOG_FILE 2>&1
+    fi
+    */
+
+    
 }
--- Tests/DockerTask/DockerTaskTests.swift
+++ Tests/DockerTask/DockerTaskTests.swift
@@ -13,52 +13,80 @@ class DockerTaskTests: XCTestCase {
 //        recordFailure(withDescription: "Forced Failure", inFile: String(#file), atLine: 19, expected: true)
 //    }
     
+    func testVMExists(){
+        let task = DockerTask(command: "")
+        XCTAssertTrue(task.vmExists(name:"default"), "Failed to find Virtual Machine ")
+    }
+    func testVMDoesNotExist(){
+        let task = DockerTask(command: "")
+        XCTAssertFalse(task.vmExists(name:String(UUID())), "Unknown VM should not have been found.")
+    }
+    func testVmIsRunning(){
+        let task = DockerTask(command: "")
+        XCTAssertTrue(task.vmIsRunning(name:"default"), "Failed to find running Virtual Machine ")
+    }
+    func testVmIsNotRunning(){
+        let task = DockerTask(command: "")
+        Task.run(launchPath:"/bin/bash", arguments: ["-c", "export PATH=/usr/local/bin:$PATH && \(task.machinePath) stop default"], silenceOutput: false)
+        XCTAssertFalse(task.vmIsRunning(name:"default"), "Machine should not be running")
+    }
+    
     func testContainerNameOption() {
         let name = String(UUID())
         let command = ["whoami"]
 
-        let result = DockerTask(command:"run", commandOptions:["--name", name, "--rm"], imageName:imageName, commandArgs:command).launch()
-        
-        XCTAssertEqual(result.exitCode, 0)
-        XCTAssertNil(result.error)
-        XCTAssertNotNil(result.output)
-        
-        
+        do{
+            let result = try DockerTask(command:"run", commandOptions:["--name", name, "--rm"], imageName:imageName, commandArgs:command).launchFromToolbox(silenceOutput: false)
+            
+            XCTAssertEqual(result.exitCode, 0)
+            XCTAssertNil(result.error)
+            XCTAssertNotNil(result.output)
+        }catch let e{
+            XCTFail("\(e)")
+        }
     }
     func testDeleteExistingContainer(){
         var containerName = #function
         let range = containerName.index(containerName.endIndex, offsetBy: -2)..<containerName.endIndex
         containerName.removeSubrange(range)
         
-        //create the container
-        DockerTask(command:"run", commandOptions:["--name", containerName], imageName:imageName, commandArgs:command).launch(silenceOutput: true)
-        let creationResult = DockerTask(command:"ps", commandOptions:["-a"]).launch(silenceOutput: true)
-        XCTAssertNotNil(creationResult.output)
-        XCTAssertNotNil(creationResult.output!.range(of: containerName))
-        
-        
-        //delete the container
-        DockerTask(command:"rm", commandOptions:[containerName]).launch(silenceOutput: true)
-        //check if it exists
-        let result = DockerTask(command:"ps", commandOptions:["-a"]).launch(silenceOutput: true)
-        
-        XCTAssertEqual(result.exitCode, 0)
-        XCTAssertNil(result.error)
-        XCTAssertNotNil(result.output)
-        XCTAssertNil(result.output!.range(of: containerName))
+        do{
+            //create the container
+            try DockerTask(command:"run", commandOptions:["--name", containerName], imageName:imageName, commandArgs:command).launchFromToolbox(silenceOutput: false)
+            let creationResult = try DockerTask(command:"ps", commandOptions:["-a"]).launchFromToolbox(silenceOutput: false)
+            XCTAssertNotNil(creationResult.output)
+            XCTAssertNotNil(creationResult.output!.range(of: containerName))
+            
+            
+            //delete the container
+            try DockerTask(command:"rm", commandOptions:[containerName]).launchFromToolbox(silenceOutput: false)
+            //check if it exists
+            let result = try DockerTask(command:"ps", commandOptions:["-a"]).launchFromToolbox(silenceOutput: false)
+            
+            XCTAssertEqual(result.exitCode, 0)
+            XCTAssertNil(result.error)
+            XCTAssertNotNil(result.output)
+            XCTAssertNil(result.output!.range(of: containerName))
+        }catch let e{
+            XCTFail("\(e)")
+        }
     }
     
     func testDoesRunInDocker(){
-        let macHostname = Task.run(launchPath:"/bin/hostname", arguments: nil).output?.trimmingCharacters(in:NSMutableCharacterSet.newline() as CharacterSet)
-        let name = String(UUID())
-        let hostNameCommand = "if [ `hostname` == \(name) ]; then echo \(name); fi"
+        do{
+            let macHostname = Task.run(launchPath:"/bin/hostname", arguments: nil).output?.trimmingCharacters(in:NSMutableCharacterSet.newline() as CharacterSet)
+            let name = String(UUID())
+            let hostNameCommand = "hostname"//can't call $(hostname) or `hostname` because the macOS interprets it before docker ; if [ `hostname` == \(name) ]; then echo \(name); fi.
+            
+            let linuxResult = try DockerTask(command: "run", commandOptions: ["--name", name, "--rm", "--hostname", name], imageName: imageName, commandArgs: ["/bin/bash", "-c", hostNameCommand]).launchFromToolbox(silenceOutput: false)
         
-        let linuxResult = DockerTask(command: "run", commandOptions: ["--name", name, "--rm", "--hostname", name], imageName: imageName, commandArgs: ["/bin/bash", "-c", hostNameCommand]).launch()
-    
-        let linuxHostname = linuxResult.output?.trimmingCharacters(in:NSMutableCharacterSet.newline() as CharacterSet)
-        XCTAssertEqual(linuxHostname, name)
-        XCTAssertNotEqual(linuxHostname, macHostname)
-        XCTAssertEqual(linuxResult.exitCode, 0)
+            let linuxHostname = linuxResult.output?.trimmingCharacters(in:NSMutableCharacterSet.newline() as CharacterSet)
+            XCTAssertEqual(linuxHostname, name)
+            XCTAssertNotEqual(linuxHostname, macHostname)
+            XCTAssertEqual(linuxResult.exitCode, 0)
+        }catch let e{
+            XCTFail("\(e)")
+        }
     }
     
     static var allTests : [(String, (DockerTaskTests) -> () throws -> Void)] {
