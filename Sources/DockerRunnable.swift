eaabcd546edb6a8514b2c88313c815ac31f7aac3
Joel Saltzman <talk@joelsaltzman.com>
2016-12-21 23:13:31 -0800

    added launch function to DockerRunnable

--- Sources/DockerRunnable.swift
+++ Sources/DockerRunnable.swift
@@ -7,7 +7,10 @@
 //
 
 import Foundation
+import SynchronousProcess
 
 public protocol DockerRunnable {
     init(command: String, commandOptions: [String]?, imageName: String?, commandArgs: [String]?)
+    @discardableResult
+    func launch(silenceOutput:Bool) -> ProcessResult
 }
